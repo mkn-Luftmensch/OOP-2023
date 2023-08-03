57db5dce66bc3a6d66f23c8cc0e8ac56dfea4543
mkn-Luftmensch <a1902103@adelaide.edu.au>
2023-08-03 14:42:00 +0930

    ez

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -7,5 +7,5 @@ void count_digits(int array[4][4]){
         for(int j=0; j<4; j++) 
             frequency[array[i][j]]++;
 
-    cout << "0:" << frequency[0] << ";1:" << frequency[1] << ";2:" << frequency[2] << ";3:" << frequency[3] << ";4:" << frequency[4] << ";5:" << frequency[5] << ";6:" << frequency[6] << ";7:" << frequency[7] << ";8:" << frequency[8] << ";9:" << frequency[9];
+    cout << "0:" << frequency[0] << ";1:" << frequency[1] << ";2:" << frequency[2] << ";3:" << frequency[3] << ";4:" << frequency[4] << ";5:" << frequency[5] << ";6:" << frequency[6] << ";7:" << frequency[7] << ";8:" << frequency[8] << ";9:" << frequency[9] << ";";
 }
