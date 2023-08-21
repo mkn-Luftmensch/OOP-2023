85534b753c6b173c0cdcadd4c2b173ccec34cb63
mkn-Luftmensch <a1902103@adelaide.edu.au>
2023-08-22 09:24:45 +0930

    not so ez

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -1,9 +1,32 @@
 #include "Orchestra.h"
 #include "Musician.h"
 
-Orchestra::Orchestra() : _size(0) {}
-Orchestra::Orchestra(int size) : _size(size) {}
-int Orchestra::get_current_number_of_members() {}
+Orchestra::Orchestra() : _size(0), current_members(0) {}
+Orchestra::Orchestra(int size) : _size(size), current_members(0) {
+    _members = new Musician[_size];
+}
+int Orchestra::get_current_number_of_members() {
+    return current_members;
+}
 bool Orchestra::has_instrument(std::string instrument) {
-    
+    for(int i=0; i<_size; i++){
+        if(_members[i].get_instrument() == instrument) return true;
+    }
+    return false; 
+}
+Musician* Orchestra::get_members(){
+    return _members;
+}
+bool Orchestra::add_musician(Musician new_musician){
+    if(current_members < _size) {
+        _members[current_members] = new_musician;
+        current_members++;
+        return true;
+    }
+    else return false;
+
+}
+
+Orchestra::~Orchestra(){
+    delete[] _members;
 }
--- Orchestra.h
+++ Orchestra.h
@@ -7,14 +7,15 @@ class Orchestra {
     private:
         int _size;
         std::string _instrument;
-        Musician* _members = new Musician[_size];
+        Musician* _members;
+        int current_members;
     public:
         Orchestra();
         Orchestra(int size);
         int get_current_number_of_members();
         bool has_instrument(std::string instrument);
         Musician* get_members();
-        bool add_musicians(Musician new_musician);
+        bool add_musician(Musician new_musician);
         ~Orchestra();
 }; 
 
