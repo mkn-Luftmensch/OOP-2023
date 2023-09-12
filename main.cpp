be2be91906228975b446759ad3ae7f9450092492
mkn-Luftmensch <a1902103@adelaide.edu.au>
2023-09-12 23:25:52 +0930

    dmgs

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 int main() {
-    Gradebook gradebook(10);
-    Course COMP_SCI(22, "Object Oriented Programming", 1106);
-    Student Mike(2103, "Mai Khoi Nguyen");
-    COMP_SCI.addPerson(&Mike);
-    Grade mknGrade(2103, 1106, "OOP", 100);
-    gradebook.addGrade(2103, 1106, "OOP", 100);
-    University UoA(17, "University of Adelaide", "Adelaide");
-    // UoA.addCourse(22, 1106, "Object Oriented Programming");
+    // Gradebook gradebook(10);
+    // Course COMP_SCI(22, "Object Oriented Programming", 1106);
+    // Student Mike(2103, "Mai Khoi Nguyen");
+    // COMP_SCI.addPerson(&Mike);
+    // Grade mknGrade(2103, 1106, "OOP", 100);
+    // gradebook.addGrade(2103, 1106, "OOP", 100);
+    // University UoA(17, "University of Adelaide", "Adelaide");
+    // // UoA.addCourse(22, 1106, "Object Oriented Programming");
 
     
     return 0;
